668180659b905cb4d06b2d20b58ecb3545d0032a
Pankau-p <pankaurealestate@outlook.com>
2026-04-17 01:01:38 -0400

    Controller for add_product and errors

--- product_manager/index.php
+++ product_manager/index.php
@@ -3,28 +3,47 @@
 require_once('../model/database.php');
 require_once('../model/product_db.php');
 
+$error = null;
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $products = get_products($db);
-
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    // var_dump($_POST);
-    // exit;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
     $action = $_POST['action'] ?? '';
 
     if ($action === 'delete_product') {
+        
         $product_code = $_POST['product_code'] ?? '';
 
         if (!empty($product_code)) {
             delete_product($db, $product_code);
+        } else {
+            $error = "Invalid product selected";
+        }
+
+    } elseif ($action === 'add_product') {
+        
+        $product_code = $_POST['product_code'] ?? '';
+        $name = $_POST['name'] ?? '';
+        $version = $_POST['version'] ?? '';
+        $releaseDate = $_POST['releaseDate'] ?? '';
+
+        if (!empty($product_code) && !empty($name) &&
+            !empty($version) && !empty($releaseDate)) {
+            
+            add_product($db, $product_code, $name, $version, $releaseDate);
+
+        } else {
+            $error = "Invalid product data. Check all fields and try again.";
         }
     }
-    $products = get_products($db);
 }
+
+// Refresh state to get updates
+$products = get_products($db);
+
 
 // render page
 include('../view/header.php');
+include('../view/error.php');
 include('../view/product_list.php');
 include('../view/footer.php');
 ?>
---
+++ view/error.php
@@ -0,0 +1,5 @@
+<?php if (!empty($error)) : ?>
+    <div style="color: red; margin: 10px 0;">
+        <?php echo $error; ?>
+    </div>
+<?php endif; ?>
