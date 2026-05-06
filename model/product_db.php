83a7632db1fa638bc4148aa888365a17d79af3f4
Pankau-p <pankaurealestate@outlook.com>
2026-05-06 19:51:48 -0400

    Starting to convert model to OOP

--- model/product_db.php
+++ model/product_db.php
@@ -11,26 +11,45 @@
 // Handles getting, deleting, and adding a product. 
 
 
-// Get all products
-function get_products($db) {
-    $query = 'SELECT * FROM products ORDER BY productCode';
-    $statement = $db->prepare($query);
-    $statement->execute();
-    $products = $statement->fetchAll();
-    $statement->closeCursor();
-    return $products;
-}
+class ProductDB {
+    private $db;
 
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+// Get all products
+    public function get_products(){
+        try {
+            $query = 'SELECT * FROM products ORDER BY productCode';
+            $statement = $this->db->prepare($query);
+            $statement->execute();
+            $products = $statement->fetchAll();
+            $statement->closeCursor();
+            return $products;
+        } catch (PDOException $e) {
+            $error_message = $e->getMessage();
+            include ('../view/shared/database_error.php');
+            exit();
+        }
+    }
+    
 // Delete one product with product code
-function delete_product($db, $product_code) {
-    $query = 'DELETE FROM products 
-              WHERE productCode = :product_code';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':product_code', $product_code);
-    $success = $statement->execute();
-    $statement->closeCursor();
-    return $success;
-}
+    public function delete_product($product_code){
+        try {
+            $query = 'DELETE FROM products 
+                      WHERE productCode = :product_code';    
+            $statement = $this->db->prepare($query);
+            $statement->bindValue(':product_code', $product_code);
+            $success = $statement->execute();
+            $statement->closeCursor();
+            return $success;
+        } catch (PDOException $e) {
+            $error_message= $e->getMessage();
+            include('../view/shared/database_error.php');
+            exit();
+        }
+    }
 
 // Add a product with productCode, name, version, and releaseDate
 function add_product($db, $product_code, $name, $version, $releaseDate) {
@@ -46,6 +65,7 @@ function add_product($db, $product_code, $name, $version, $releaseDate) {
     $success = $statement->execute();
     $statement->closeCursor();
     return $success;
+}
 }
 ?>
 
